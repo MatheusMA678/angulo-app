bc46dee0dbaf9cb4c8297d34db931f970df6a7f9
David Cardoso <davidcmelo99@hotmail.com>
2023-08-03 22:05:21 -0300

    up styles

--- src/Components/planos.tsx
+++ src/Components/planos.tsx
@@ -1,6 +1,6 @@
 
 'use client'
-import "../styles/globals.css";
+import "./styles/globals.css";
 import { Button, ButtonGroup, CardMedia } from '@mui/material';
 import { useEffect, useState } from "react";
 import React from "react";
